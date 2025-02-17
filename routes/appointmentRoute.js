8ffefb6d055038c4bd4ef824e22d2a25797c9ef8
Anantha1013 <nmananthalakshmy@gmail.com>
2025-02-17 22:54:15 +0530

    Swagger update

--- routes/appointmentRoute.js
+++ routes/appointmentRoute.js
@@ -6,6 +6,35 @@ const router = express.Router();
 /**
  * @swagger
  * tags:
+ *   name: Departments
+ *   description: Department API
+ */
+
+/**
+ * @swagger
+ * /departments:
+ *   get:
+ *     summary: Get the list of available departments in the hospital
+ *     tags: [Departments]
+ *     responses:
+ *       200:
+ *         description: Successfully fetched all departments
+ *         content:
+ *           application/json:
+ *             schema:
+ *              type: object
+ *              properties:
+ *                message:
+ *                  type: string
+ *                departments:
+ *                  type: array
+ *       505:
+ *         description: Some error in the server side      
+ */
+
+/**
+ * @swagger
+ * tags:
  *   name: Doctors
  *   description: Doctors API
  */
@@ -16,6 +45,19 @@ const router = express.Router();
  *   get:
  *     summary: Get list of available doctors
  *     tags: [Doctors]
+ *     parameters:
+ *       - name: appointmentDate
+ *         in: path
+ *         description: Format YYYY-MM-DD
+ *         required: true
+ *       - name: appointmentTime
+ *         in: path
+ *         description: Format HH:MM
+ *         required: true
+ *       - name: dept_name
+ *         in: path
+ *         description: Any valid department name within in the hospital
+ *         required: true
  *     responses:
  *       200:
  *         description: Successfully fetched the list of doctors
@@ -55,8 +97,16 @@ const router = express.Router();
  *             properties:
  *               doctorId:
  *                 type: string
+ *               patientName:
+ *                 type: string
  *               patientId:
  *                 type: string
+ *               patientType:
+ *                 type: string
+ *                 enum: [in-patient, out-patient]
+ *               bedType:
+ *                 type: string
+ *                 enum: [general]
  *               appointmentDate:
  *                 type: string
  *                 format: date
@@ -72,6 +122,22 @@ const router = express.Router();
  *               properties:
  *                 message:
  *                   type: string
+ *                 appointment:
+ *                   type: object
+ *                   properties:
+ *                     patientId:
+ *                       type: string
+ *                     patientName:
+ *                       type: string
+ *                     appointmentDate:
+ *                       type: string
+ *                       format: date
+ *                     appointmentTime:
+ *                       type: string
+ *                 bedDetails:
+ *                   type: object
+ *                   properties:
+ *                     bedId: string
  *       500:
  *         description: Some server error
  */
